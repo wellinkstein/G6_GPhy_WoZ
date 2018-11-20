636098f2096126bd936fa6a96558b3ea6ab902c7
Jérémie <guilbaud.jeremie@tutanota.com>
2018-11-20 16:50:33 +0100

    init

---
+++ Fight.java
@@ -0,0 +1,50 @@
+
+/**
+ * Décrivez votre classe Fight ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Fight
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private boolean Fighter;
+
+    /**
+     * Constructeur d'objets de classe Fight
+     */
+    public Fight()
+    {
+
+    }
+
+    /**
+     * 
+     */
+    public boolean firstFighter()
+    {return true;}
+    
+    /**
+     * 
+     */
+    public boolean playerAttack()
+    {return true;}
+    
+    /**
+     * 
+     */
+    public boolean mosnterAttack()
+    {return true;}
+    
+    /**
+     * 
+     */
+    public void fight()
+    {}
+    
+    /**
+     * 
+     */
+    public void endFight()
+    {}
+}
