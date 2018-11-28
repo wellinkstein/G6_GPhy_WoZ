994a208e0befae4916c890214b527f3b804e388a
Yousra <moughit-yousra@hotmail.fr>
2018-11-28 15:47:32 +0100

    Item

--- Common.java
+++ Common.java
@@ -6,7 +6,12 @@
  */
 public class Common extends Item
 {
-
+     private int damage; //dictates the damage attributed to an item
+     private int protection;
+     protected String name; 
+     private String description; 
+     private int healthGain; //determines the health an item will give to the player when the item is used
+     
     /**
      * Constructor of a common item
      */
@@ -15,5 +20,63 @@ public class Common extends Item
         super (itemName, des, dam, HG, prot);
     
     }
+    /**
+     * setter for the damage
+     * the damage can be equal to 0 but cannot be negative
+     * @param dam: value of the damage of the item
+     */
+     public void setDamage (int dam)
+    {
+        if (dam>=0) //If the value of the damage is over or equal to 0
+        {
+            damage = dam; //The value of the damage does  change
+        }
+     }
+    
+     /**
+     * setter for the protection
+     * the protection can be equal to 0 but cannot be negative
+     * @param prot: the value of protection of the item
+     */
+    public void setProtection(int prot)
+    {
+        if (prot>=0) //If the value of the protection is over or equal to 0
+        {
+            protection = prot;  //The value of the protection does change
+        }
+    }
+    
+     /**
+     * setter for the name
+     * @param myName: the name of the item
+     */
+    public void setName(String myName)
+    {
+        name = myName;
+    }
+    
+     /**
+     * A mothod to set the description of the item
+     * @param String description: the description of the item
+     */
+    public void setDescription(String description)
+    {
+        this.description=description;
+    }
+    
+    /**
+     * A method to set the value of healh given by the item (potion)
+     * This value cannot be set under 0
+     * @param int healthGain: integer of the value of health gain
+     */
+    
+    public void setHealthGain(int healthGain)
+    {
+        if(healthGain>=0)
+        {
+            this.healthGain = healthGain;
+        }
+    }
+   
 
 }
--- LegendaryTest.java
+++ LegendaryTest.java
@@ -14,6 +14,7 @@ public class LegendaryTest
 {
     //The necessary attribute/variable to test the functions of this class
     private Legendary goodLegendary;
+    private Item goodItem;
 
     /**
      * Constructor of the legendary test class
@@ -41,6 +42,83 @@ public class LegendaryTest
         assertEquals(2,goodLegendary.getDamage());
         assertEquals (2,goodLegendary.getProtection());
         assertEquals (0,goodLegendary.getHealthGain());
+    }
+    
+     /**
+     * Test that checks if the getters work properly
+     */
+    //@Test
+    //public void testGoodItem()
+    {
+        //assertEquals("Iron Sword",goodItem.getName());
+        //assertEquals("Common sword",goodItem.getDescription());
+        //assertEquals(2,goodItem.getDamage());
+        //assertEquals (2,goodItem.getProtection());
+        //assertEquals (0,goodItem.getHealthGain());
+    }
+    
+    /**
+     * Test if it is possible to set a negative value for healGain
+     */
+    @Test
+    public void testSetNegativeHPGain()
+    {
+        goodLegendary.setHealthGain(-10);
+        assertEquals(0, goodLegendary.getHealthGain()); // the value of healthGain should remain at 0
+    }
+    
+    
+     /**
+     * test if the set damage accepts a negative value
+     * this should not be possible
+     */
+    @Test
+    public void testSetNegativeDamage()
+    {
+        goodLegendary.setDamage(-10);//Damage with a negative value
+        assertEquals(2,goodLegendary.getDamage());//The parameter "2" must remain defined as damage attribute
+    }
+    
+    /**
+     * test if the set damage method accepts a positive value
+     * This should be possible
+     */
+    @Test
+    public void testSetPositiveDamage()
+    {
+        goodLegendary.setDamage(10);//Damage with a positive value
+        assertEquals(10,goodLegendary.getDamage());//The parameter "10" must be set as the damage attribute
+    }
+    /**
+     * tests if the set protection method accepts a negative value
+     * This should not be possible
+     */
+    @Test
+    public void testSetNegativeProtection()
+    {
+        goodLegendary.setProtection(-10);//Protection with a negative value
+        assertEquals(2,goodLegendary.getProtection());//The parameter "2" must remain defined as protection attribute
+    }
+    
+    /**
+     * tests if the set protection method accepts a positive value
+     * this should be possible
+     */
+    @Test
+    public void testSetPositiveProtection()
+    {
+        goodLegendary.setProtection(10);//Protection with a positive value
+        assertEquals(10,goodLegendary.getProtection());//The parameter "10" must be set as the protection attribute
+    }
+    /**
+     * test for good name
+     *  Checks if the name given as parameter in the constructor is correctly set to the attribute
+     */
+    @Test
+    public void testSetName()
+    {
+        goodLegendary.setName("YousraDelphine");
+        assertEquals("YousraDelphine",goodLegendary.getName()); // The parameter "YousraDelphine" must be set as the name attribute
     }
 
     /**
