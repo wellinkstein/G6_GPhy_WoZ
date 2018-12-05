dbb98b3e6256694c3452811565e27f5df4f28711
Jérémie <guilbaud.jeremie@tutanota.com>
2018-12-05 17:35:24 +0100

    vrwhxfh

--- fightInterface.java
+++ fightInterface.java
@@ -10,12 +10,14 @@ import java.awt.event.*;
  */
 public class fightInterface extends JFrame implements ActionListener
 {
-    
+
     private JButton buttonStartFight;
     private JPanel panelMain, panelBot, panelBig;
     private JLabel dialog, playerImg, monsterImg;
     private JFrame myFrame;
     private ImageIcon iconPlayer, iconMonster;
+    private Game myGame;
+    private Spot mySpot;
 
     /**
      * Constructor of objects class fightInterface
@@ -23,72 +25,76 @@ public class fightInterface extends JFrame implements ActionListener
     public fightInterface()
     {
         myFrame = new JFrame("Fight Interface");
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        myGame = new Game();
+        mySpot = new Spot();
+
         buttonStartFight = new JButton("Start Fight");
         buttonStartFight.addActionListener(this);
-        
+
         dialog = new JLabel("Wow much damage very suspense");
-        
+
         panelBot = new JPanel();
         panelBot.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        
+
         c.fill = GridBagConstraints.HORIZONTAL;
         c.weightx = 0.95;
         c.gridx = 0;
         c.gridy=0;
         panelBot.add(dialog, c);
-        
+
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.gridy = 0; 
         c.weightx = 0.05;
         panelBot.add(buttonStartFight, c);
         
+        panelMain = new JPanel();
+        panelMain.setLayout(new GridBagLayout());
+        
         iconPlayer = new ImageIcon("player.jpg"); 
         playerImg = new JLabel();
         playerImg.setIcon(iconPlayer);
         
-        iconMonster = new ImageIcon("darth_vader.jpg");
-        monsterImg = new JLabel();
-        monsterImg.setIcon(iconMonster);
-        
-        panelMain = new JPanel();
-        panelMain.setLayout(new GridBagLayout());
-        
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 0;
-        c.gridy=0;
-        panelMain.add(playerImg,c);
-        
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 1;
-        c.gridy=0;
-        panelMain.add(monsterImg,c);
-        
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.weightx = 0.5;
+            c.gridx = 0;
+            c.gridy=0;
+            panelMain.add(playerImg,c);
 
+        if (myGame.getCurrentSpot().getMonster() != null)
+        {
+
+            iconMonster = new ImageIcon("darth_vader.jpg");
+            monsterImg = new JLabel();
+            monsterImg.setIcon(iconMonster);
+            
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.weightx = 0.5;
+            c.gridx = 1;
+            c.gridy=0;
+            panelMain.add(monsterImg,c);
+        }
+        
         panelBig = new JPanel(new GridLayout(2,0));
         panelBig.add(panelMain);
         panelBig.add(panelBot);
-        
         myFrame.add(panelBig);
-        
         myFrame.pack();
         myFrame.setVisible(true);
     }
-    
+
     public void actionPerformed(ActionEvent e)
     {
-       // Game.fight();
+        myGame.fight();
     }
-    
+
     public void displayFight()
     {
         //dialog.setText("");
     }
-    
+
     //public void fighters
 }
