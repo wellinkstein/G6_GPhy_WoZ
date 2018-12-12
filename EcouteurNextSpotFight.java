17709df04ca3eadc7e040124057fd5e0bbd79022
Yohan <yohan.lefol@gmail.com>
2018-12-12 12:01:04 +0100

    update

---
+++ EcouteurNextSpotFight.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+/**
+ * This is the external listener for the fight button of the next spot class.
+ *
+ * @author (Yohan Lefol)
+ * @version (12/12/2018)
+ */
+public class EcouteurNextSpotFight implements ActionListener
+{
+    private AffichMainYohan affichMain1;
+    private Game myGame;
+
+    /**
+     * Constructor for objects of class EcouteurNextSpotFight
+     */
+    public EcouteurNextSpotFight()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void actionPerformed (ActionEvent e)
+    {
+       Game.move(Spot spot);
+    }
+}
