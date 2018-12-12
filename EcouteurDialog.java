5fff068365b449e8929e3bcbe1a713d285e4fc4b
HatimRocket <htm.rrbb@gmail.com>
2018-12-12 15:10:19 +0100

    qdf

---
+++ EcouteurDialog.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class EcouteurDialog here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class EcouteurDialog
+{
+    private AffichMainYohan affichMain;
+    private DialogScreenInterface dialog;
+    
+
+    /**
+     * Constructor for objects of class EcouteurInventory
+     */
+    public EcouteurDialog(DialogScreenInterface myDialog, AffichMainYohan myMain){
+       affichMain = myMain;
+       dialog = myDialog;
+    }
+    
+     public void actionPerformed (ActionEvent e)
+    {
+       
+        {
+            if(e.getSource() == myInventory.getContent().get(i))
+           
+        }
+    }
+    }
