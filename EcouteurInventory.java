c3e74b5de193f0f4590d577efcc615fe16689380
ludivine <ludivine.harault@gmail.com>
2018-12-13 17:03:55 +0100

    List dans inventory

--- EcouteurInventory.java
+++ EcouteurInventory.java
@@ -1,6 +1,13 @@
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.event.*;
+import java.util.*; 
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 /**
  * Write a description of class EcouteurInventory here.
  *
@@ -17,49 +24,77 @@ public class EcouteurInventory implements ActionListener
      * Constructor for objects of class EcouteurInventory
      */
     public EcouteurInventory(Inventory newInventory, AffichMainYohan myMain){
-       affichMain = myMain;
-       myInventory = newInventory;
+        affichMain = myMain;
+        myInventory = newInventory;
     }
-    
-     public void actionPerformed (ActionEvent e)
+
+    public void actionPerformed (ActionEvent e)
     {
         Container mycont = myInventory.getParent();
-        for(int i = 0; i <= ( myInventory.getContent().size()-1); i++)
+        // for(int i = 0; i <= ( myInventory.getContent().size()-1); i++)
+        // {
+            // if(e.getSource() == myInventory.getContent().get(i))
+            // {
+                // if((i%2)==0)
+                // {   
+                    // System.out.println(myInventory.getPlayer().getItems((i/2)));
+                    // System.out.println("Description of the item"+(i%2));
+                    // ItemDescription myDes = new ItemDescription(myInventory.getPlayer().getItems((i/2)));
+                    // affichMain.setDes(myDes);
+                // }
+                // else
+                // {
+                    // // System.out.println("Je lache un truc"); 
+                    // // System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
+                    // // System.out.println(myInventory.getPlayer().getItems((i/2)).getName());
+
+                    // affichMain.getGame().getListSpot().get(19).addItemSpot(myInventory.getPlayer().getItems((i/2)));
+                    // //System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
+
+                    // affichMain.getGame().getPlayer().dropItem(myInventory.getPlayer().getItems((i/2)));
+                    // //System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
+
+                    // affichMain.getInventory().showPlayerItem(affichMain.getGame().getPlayer(),affichMain);
+                    // // System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
+
+                    // affichMain.getSpotItem().showListItem(affichMain.getGame().getListSpot().get(19),affichMain);
+                    // //System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
+
+                    // //PlayerHead playerH = new PlayerHead(myInventory.getPlayer());
+                    // //affichMain.setPlayerHead(playerH);
+                    // affichMain.getPlayerHead().showPlayerHead(affichMain.getGame().getPlayer()); 
+
+                    // //System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
+                // }
+            // }
+        // }
+        for(int i = 0; i <= (affichMain.getGame().getPlayer().getNumberItemPossess()); i++)
         {
-            if(e.getSource() == myInventory.getContent().get(i))
-            {
-                if((i%2)==0)
-                {   
-                    System.out.println(myInventory.getPlayer().getItems((i/2)));
-                    System.out.println("Description of the item"+(i%2));
-                    ItemDescription myDes = new ItemDescription(myInventory.getPlayer().getItems((i/2)));
+            if(e.getSource() == myInventory.getCombo())
+            {  
+                    System.out.println(myInventory.getPlayer().getItems(myInventory.getCombo().getSelectedIndex()));
+                    ItemDescription myDes = new ItemDescription(myInventory.getPlayer().getItems(myInventory.getCombo().getSelectedIndex()));
                     affichMain.setDes(myDes);
+
                 }
-                else
-                {
-                    // System.out.println("Je lache un truc"); 
-                    // System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
-                    // System.out.println(myInventory.getPlayer().getItems((i/2)).getName());
-                    
-                    affichMain.getGame().getListSpot().get(19).addItemSpot(myInventory.getPlayer().getItems((i/2)));
-                    //System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
-                    
-                    affichMain.getGame().getPlayer().dropItem(myInventory.getPlayer().getItems((i/2)));
-                    //System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
-                    
+            else if (e.getSource()== myInventory.getMyButton())
+            {
+
+                    affichMain.getGame().getCurrentSpot().addItemSpot(myInventory.getPlayer().getItems(myInventory.getCombo().getSelectedIndex()));
+
+                    affichMain.getGame().getPlayer().dropItem(myInventory.getPlayer().getItems(myInventory.getCombo().getSelectedIndex()));
+
                     affichMain.getInventory().showPlayerItem(affichMain.getGame().getPlayer(),affichMain);
-                   // System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
-                    
-                    affichMain.getSpotItem().showListItem(affichMain.getGame().getListSpot().get(19),affichMain);
+                    // System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
+                    affichMain.getSpotItem().showListItem(affichMain.getGame().getCurrentSpot(),affichMain);
                     //System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
-                    
                     //PlayerHead playerH = new PlayerHead(myInventory.getPlayer());
                     //affichMain.setPlayerHead(playerH);
                     affichMain.getPlayerHead().showPlayerHead(affichMain.getGame().getPlayer()); 
-                    
+
                     //System.out.println(affichMain.getGame().getPlayer().getNumberItemPossess());
                 }
             }
         }
     }
-}
+
--- Inventory.java
+++ Inventory.java
@@ -10,7 +10,7 @@ import java.util.Set;
 /**
  * This class is an example of GridLayout Manager
  * 
- * @author Corentin Journay 
+ * @author Groupe 7  
  * @version 04/12/2018
  */
 public class Inventory extends JPanel
@@ -21,7 +21,9 @@ public class Inventory extends JPanel
     private JLabel myLabel; 
     private JPanel myPanel, myPanel1, myBigPanel; 
     private Spot spot;
+    private JComboBox combo; 
     private AffichMainYohan myMain;
+    private JButton myButton; 
 
     public Inventory(Player theseus, AffichMainYohan cont)
     {
@@ -36,43 +38,50 @@ public class Inventory extends JPanel
         
         JPanel myPanel;
         JLabel myLabel; 
-        // myFrame = new JFrame("Inventory");
-        // myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // myFrame.setLayout(new GridLayout(0,1));
+
         myPanel = new JPanel();
         myPanel1= new JPanel(); 
         myBigPanel=new JPanel();
-        
         myBigPanel.removeAll();
         myPanel.removeAll();
         myPanel1.removeAll();
         content.clear();
         
+        myButton = new JButton("Drop"); 
+        combo = new JComboBox(); 
+        EcouteurInventory e = new EcouteurInventory(this, myMain);
+        
         for(int i = 0; i <= (monJoueur.getNumberItemPossess()-1); i++)
         {
-            content.add(new JButton(monJoueur.getItems(i).getName()));
-            content.add(new JButton("Drop Item"));
+           // content.add(new JButton(monJoueur.getItems(i).getName()));
+           // content.add(new JButton("Drop Item"));
+           combo.addItem(monJoueur.getItems(i).getName());
+           combo.addActionListener(e); 
+           
         }
 
-        
+        myButton.addActionListener(e); 
         myLabel = new JLabel ("Your inventory",JLabel.CENTER);
-        myPanel.setLayout(new GridLayout(2,monJoueur.getNumberItemPossess()));
-        myPanel1.setLayout(new GridLayout(1,1));
+        //myPanel.setLayout(new GridLayout(2,monJoueur.getNumberItemPossess()));
+        myPanel.setLayout(new GridLayout(1,monJoueur.getNumberItemPossess()));
+        myPanel1.setLayout(new GridLayout(1,0));
         myPanel1.add(myLabel); 
+        myPanel.add(combo); 
+        myPanel.add(myButton); 
 
-        for(int i = 0; i <= (content.size()-1); i+=2)
-        {
-            EcouteurInventory e = new EcouteurInventory(this, myMain);
-            content.get(i).addActionListener(e);
-            myPanel.add(content.get(i));
-        }
+        // for(int i = 0; i <= (content.size()-1); i+=2)
+        // {
+            // EcouteurInventory e = new EcouteurInventory(this, myMain);
+            // content.get(i).addActionListener(e);
+            // myPanel.add(content.get(i));
+        // }
         
-        for(int i = 1; i <= (content.size()-1); i+=2)
-        {
-            EcouteurInventory e = new EcouteurInventory(this, myMain);
-            content.get(i).addActionListener(e);
-            myPanel.add(content.get(i));
-        }
+        // for(int i = 1; i <= (content.size()-1); i+=2)
+        // {
+            // EcouteurInventory e = new EcouteurInventory(this, myMain);
+            // content.get(i).addActionListener(e);
+            // myPanel.add(content.get(i));
+        // }
 
         myBigPanel.setLayout(new GridLayout(2,1)); 
         myBigPanel.add(myPanel1); 
@@ -95,5 +104,13 @@ public class Inventory extends JPanel
     public ArrayList<JButton> getContent(){
         return content;
     }
-   
+    
+    public JButton getMyButton()
+    {
+        return myButton; 
+    }
+    
+    public JComboBox getCombo(){
+        return combo; 
+    }
 }
