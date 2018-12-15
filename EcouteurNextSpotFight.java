849f37e1f8622c033417ba6d7f6ab42558ef15a1
UchiwaM <journay.corentin@gmail.Com>
2018-12-15 15:55:18 +0100

    oui

--- EcouteurNextSpotFight.java
+++ EcouteurNextSpotFight.java
@@ -35,9 +35,9 @@ public class EcouteurNextSpotFight implements ActionListener
         }
         else if (e.getSource()==myNextSpot.getMyFightButton())
         {
-            System.out.println(affichMain1.getGame().getCurrentSpot().getExits(affichMain1.getNextSpot().getDirection()));
+            //System.out.println(affichMain1.getGame().getCurrentSpot().getExits(affichMain1.getNextSpot().getDirection()));
             affichMain1.getGame().move(affichMain1.getGame().getCurrentSpot().getExits(affichMain1.getNextSpot().getDirection()));
-             affichMain1.getMonsterHead().showMonsterHead(affichMain1.getGame().getCurrentSpot().getMonster()); 
+            affichMain1.getMonsterHead().showMonsterHead(affichMain1.getGame().getCurrentSpot().getMonster()); 
             affichMain1.getNextSpot().showNextSpot(affichMain1.getGame().getCurrentSpot(), affichMain1,affichMain1.getNextSpot().getDirection());
             affichMain1.setScrolling();
         }
