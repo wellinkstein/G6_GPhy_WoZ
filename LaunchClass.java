ee832ce7867bc9b09c68731bd6ed04e868322c84
Yohan <yohan.lefol@gmail.com>
2018-12-21 12:49:50 +0100

    update

---
+++ LaunchClass.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * This is the class that is used to launch the project from a .jar File
+ *
+ * @author (Yohan Lefol)
+ * @version (21/12/2018)
+ */
+public class LaunchClass
+{
+    
+    
+
+    /**
+     * Constructor for objects of class LaunchClass
+     */
+    public LaunchClass()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * The main method called from the .Jar file
+     */
+    public static void main(String[] args)
+    {
+        
+        AffichMain affich=new AffichMain();
+    }
+}
